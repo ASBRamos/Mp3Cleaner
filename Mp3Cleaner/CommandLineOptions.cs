91244c3411efdb982311b7541c0aa515762ac393
ASBRamos <andreramos.ar@gmail.com>
2023-04-13 17:47:19 -0400

    Added initial files

---
+++ Mp3Cleaner/CommandLineOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Mp3Cleaner
+{
+    internal class CommandLineOptions
+    {
+        [Option('d', "directory", Required = false, HelpText = "Target directory containing all the folders copied from phone.")]
+        public string TargetDirectory { get; set; }
+    }
+}
