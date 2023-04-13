c2dcc2cc192d2eac1346b39b91adbe1d22302e97
Andre Ramos <andre.ramos@spectrumbullpen.com>
2023-04-13 17:47:19 -0400

    Added initial files

---
+++ Mp3Cleaner/FolderManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp3Cleaner
+{
+    internal class FolderManager
+    {
+        public string FolderName { get; set; }
+
+        public FolderManager(string folderName)
+        {
+            FolderName = folderName;
+        }
+
+        public IEnumerable<FolderManager> Subdirectories()
+        {
+            return Directory.GetDirectories(FolderName).Select(x => new FolderManager(x));
+        }
+
+        public IEnumerable<FileInfo> Files()
+        {
+            return Directory.GetFiles(FolderName).Select(x => new FileInfo(x));
+        }
+
+        public bool MoveFileHere(string filePath)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(filePath);
+                File.Move(filePath, Path.Combine(FolderName, fileName));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+                Directory.Delete(FolderName, true);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool RenameFile(string fileName, string newFileName)
+        {
+            try
+            {
+                string filePath = Path.Combine(FolderName, fileName);
+                string newFilePath = Path.Combine(FolderName, newFileName);
+                File.Move(filePath, newFilePath);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Mp3Cleaner/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using CommandLine;
+
+namespace Mp3Cleaner;
+
+public class Program
+{
+    internal static long MinimumMp3SizeBytes = 20 * 1024 * 1024; // 20 MB
+
+    static void Main(string[] args)
+    {
+        string targetDirectory = string.Empty;
+        Parser.Default.ParseArguments<CommandLineOptions>(args)
+            .WithParsed<CommandLineOptions>(o =>
+            {
+                if (o.TargetDirectory != null && o.TargetDirectory.Length > 0)
+                {
+                    targetDirectory = o.TargetDirectory;
+                }
+            });
+
+        if (targetDirectory.Equals(string.Empty))
+        {
+            targetDirectory = Directory.GetCurrentDirectory();
+        }
+
+        FolderManager topLevelFolder = new FolderManager(targetDirectory);
+
+        foreach (var subFolder in topLevelFolder.Subdirectories())
+        {
+            foreach (var subFile in subFolder.Files())
+            {
+                if (subFile.Length > MinimumMp3SizeBytes)
+                {
+                    if (!topLevelFolder.MoveFileHere(subFile.FullName))
+                    {
+                        Console.WriteLine($"Error when trying to move a file into the target folder.");
+                    }
+                    topLevelFolder.RenameFile(subFile.Name, subFile.Name + ".mp3");
+                }
+            }
+            subFolder.Delete();
+        }
+    }
+}
